cccd826318adfcd3ab003b76e99b01851497df7d
savernike <mcrsrn99l13h224c@studenti.unirc.it>
2022-03-19 12:03:28 +0100

    Inserimento JTree prova per implementede methods

--- src/it/unirc/sapafi/gui/window/ImplMethodController.java
+++ src/it/unirc/sapafi/gui/window/ImplMethodController.java
@@ -4,6 +4,10 @@ import javax.swing.JInternalFrame;
 import javax.swing.JSplitPane;
 
 import it.unirc.sapafi.service.FrameService;
+import javax.swing.JTree;
+import java.awt.BorderLayout;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.DefaultMutableTreeNode;
 
 public class ImplMethodController {
 
@@ -18,6 +22,18 @@ public class ImplMethodController {
 		FrameService frameService = new FrameService();
 		frameService.getSplitPaneParent().put(internalFrameMethod, splitPane);
 		frameService.getListFrames().add(internalFrameMethod);
+		
+		JTree tree = new JTree();
+		tree.setModel(new DefaultTreeModel(
+			new DefaultMutableTreeNode("Implemented Methods") {
+				{
+					add(new DefaultMutableTreeNode("m1 (return int; int arg0)"));
+					add(new DefaultMutableTreeNode("m2"));
+					add(new DefaultMutableTreeNode("m3"));
+				}
+			}
+		));
+		internalFrameMethod.getContentPane().add(tree, BorderLayout.CENTER);
 	}
 
 }
