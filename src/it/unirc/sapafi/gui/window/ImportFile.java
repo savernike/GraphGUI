983ac2fcbb389f4e38ea90d54e65766ac7fa363a
Admin <pasqu@prat.it>
2022-03-22 11:38:46 +0100

    a

--- src/it/unirc/sapafi/gui/window/ImportFile.java
+++ src/it/unirc/sapafi/gui/window/ImportFile.java
@@ -15,32 +15,7 @@ public class ImportFile {
 		FileNameExtensionFilter filter = new FileNameExtensionFilter("JAR Files", "jar");
 		fileChooser.setFileFilter(filter);
 		int returnValue = fileChooser.showDialog(null, "Select JAR");
-		if (returnValue == JFileChooser.APPROVE_OPTION) {
-			String nameFile = fileChooser.getSelectedFile().getName();
-			String absolutePathFile = fileChooser.getSelectedFile().getAbsolutePath();
-			String absolutePathDir = absolutePathFile.substring(0, absolutePathFile.lastIndexOf("\\"));
-			String[] extension = { "jar" };
-			if (!checkExtensions(extension, nameFile)) {
-				JOptionPane.showMessageDialog(null, "Il formato selezionato è invalido");
-				System.out.println(absolutePathDir);
-				fileChooser = new JFileChooser();
-			}
-		}
-	}
-	
-	
-
-	private boolean checkExtensions(String[] extension, String nameFile) {
-		boolean res = false;
-		for (String ext : extension) {
-			int lastDot = nameFile.lastIndexOf(".");
-			String nameExt = nameFile.substring(lastDot + 1);
-			if(nameExt.equals(ext)) {
-				res = true;
-				break;
-			} 
-		}
-		return res;
+		
 	}
 
 }
