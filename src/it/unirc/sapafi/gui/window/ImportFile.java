b59ba96e723140a5de51ffa8bc004535a6346926
savernike <mcrsrn99l13h224c@studenti.unirc.it>
2022-03-22 12:19:40 +0100

    Inserimento vecchie modifiche

--- src/it/unirc/sapafi/gui/window/ImportFile.java
+++ src/it/unirc/sapafi/gui/window/ImportFile.java
@@ -1,6 +1,7 @@
 package it.unirc.sapafi.gui.window;
 
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class ImportFile {
@@ -13,7 +14,33 @@ public class ImportFile {
 		FileNameExtensionFilter filter = new FileNameExtensionFilter("JAR Files", "jar");
 		fileChooser.setFileFilter(filter);
 		int returnValue = fileChooser.showDialog(null, "Select JAR");
+		System.out.println(returnValue);
+		if (returnValue == JFileChooser.APPROVE_OPTION) {
+			String nameFile = fileChooser.getSelectedFile().getName();
+			String absolutePathFile = fileChooser.getSelectedFile().getAbsolutePath();
+			String[] extension = { "jar" };
+			if (!checkExtensions(extension, nameFile)) {
+				JOptionPane.showMessageDialog(null, "Il formato selezionato è invalido");
+				System.out.println(absolutePathFile);
+				fileChooser = new JFileChooser();
+			}
+		}
 	}
+	
+	private boolean checkExtensions(String[] extension, String nameFile) {
+		boolean res = false;
+		for (String ext : extension) {
+			int lastDot = nameFile.lastIndexOf(".");
+			String nameExt = nameFile.substring(lastDot + 1);
+			if(nameExt.equals(ext)) {
+				res = true;
+				break;
+			} 
+		}
+		return res;
+	}
+	
+	
 	
 
 }
